9d2c0f3e4c9c1527d56e6a0b93cde30c564884cf
prasankumar26 <prasanthedeveloper@gmail.com>
2024-06-09 11:10:36 +0530

    logout added

--- components/profile/UserProfileComponent.jsx
+++ components/profile/UserProfileComponent.jsx
@@ -4,37 +4,51 @@ import Avatar from '@mui/material/Avatar';
 import Stack from '@mui/material/Stack';
 import LogoutIcon from '@mui/icons-material/Logout';
 import Container from '@mui/material/Container';
-import { useRouter } from 'next/navigation';
 import Link from 'next/link'
+import { useDispatch, useSelector } from 'react-redux';
+import { logoutUser } from '@/app/features/user/userSlice';
+import { resetFilters } from '@/app/features/user/cateringFilterSlice';
+import { clearTiffinSlice } from '@/app/features/tiffin/tiffinFilterSlice';
+import toast from 'react-hot-toast';
+import { useRouter } from 'next/navigation'
 
 const UserProfileComponent = () => {
-    // const router = useRouter();
+    const { userDetails } = useSelector((state) => state.user)
+    const dispatch = useDispatch()
+    const router = useRouter()
 
-    // const handleBackClick = () => {
-    //       router.back();
-    //   };
+    const onLogout = () => {
+        localStorage.clear();
+        dispatch(logoutUser());
+        dispatch(resetFilters());
+        dispatch(clearTiffinSlice());
+        toast.success("Logout Successfull")
+        router.push('/catering')
+        window.location.reload();
+    }
+
 
     return (
         <div className="user-profile-gradient">
             <Container maxWidth="xl">
-               <Link href="/catering">
-               <KeyboardArrowLeftIcon  style={{ color: '#ffffff', fontWeight: '500', fontSize: '30px', marginBottom: '30px' }} />
-               </Link>
+                <Link href="/catering">
+                    <KeyboardArrowLeftIcon style={{ color: '#ffffff', fontWeight: '500', fontSize: '30px', marginBottom: '30px' }} />
+                </Link>
                 <Stack direction={{ xs: 'column', sm: 'row' }} justifyContent="space-between" spacing={2}>
                     <Stack direction="row" alignItems="center" spacing={2}>
-                        <Avatar
+                        {/* <Avatar
                             alt="Travis Howard"
                             src="https://mui.com/static/images/avatar/1.jpg"
                             sx={{ width: 100, height: 100 }}
-                        />
+                        /> */}
+                        <Avatar sx={{ bgcolor: '#a81e1e' }} style={{ width: 100, height: 100, fontSize: '3em' }}>{userDetails?.username?.slice(0, 1).toUpperCase()}</Avatar>
                         <div>
-                            <h2 className='up-name-profile'>Chris P. Bacon</h2>
-                            <p className='up-email'>ChrisBacon68@gmail.com</p>
-                            <p className='up-number'>8624852648</p>
+                            <h2 className='up-name-profile'>{userDetails?.username}</h2>
+                            <p className='up-number mt-1'>{userDetails?.phone_number}</p>
                         </div>
                     </Stack>
 
-                    <Stack direction="row" alignItems="center">
+                    <Stack direction="row" alignItems="center" style={{cursor: 'pointer'}} onClick={() => onLogout()}>
                         <LogoutIcon style={{ color: '#ffffff', fontWeight: '500', fontSize: '25px' }} />
                         <h6 className='up-signout'>Sign Out</h6>
                     </Stack>
