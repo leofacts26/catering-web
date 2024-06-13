4ca2d9fb03667b42f2ed942ab86efc3a5db5facd
prasankumar26 <prasanthedeveloper@gmail.com>
2024-06-13 07:56:11 +0530

    settings page

--- app/user-profile/page.jsx
+++ app/user-profile/page.jsx
@@ -6,6 +6,7 @@ import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import EditIcon from '@mui/icons-material/Edit';
+import Card from '@mui/material/Card';
 
 const CssTextField = styled(TextField)(({ theme }) => ({
     '& .MuiOutlinedInput-root': {
@@ -32,59 +33,70 @@ const page = () => {
         <>
             <h2 className="user-profile-title" style={{ marginTop: '30px' }}>Edit Profile</h2>
 
+
             <Grid container spacing={2}>
-                <Grid item xs={6}>
-                    <CssTextField
-                        id="outlined-number"
-                        placeholder="User Name Here..."
-                        variant="outlined"
-                        label="User Name Here..."
-                        className='mt-0'
-                        style={{ width: '100%' }}
-                        InputLabelProps={{
-                            style: { color: '#777777', fontSize: '14px' },
-                        }}
-                        InputProps={{
-                            style: {
-                                borderRadius: '8px',
-                                backgroundColor: '#FFFFFF',
-                            }
-                        }}
-                    />
-                </Grid>
-                <Grid item xs={6}>
-                    <CssTextField
-                        id="outlined-number"
-                        placeholder="Phone Number..."
-                        variant="outlined"
-                        label="Phone Number..."
-                        className='mt-0'
-                        style={{ width: '100%' }}
-                        InputLabelProps={{
-                            style: { color: '#777777', fontSize: '14px' },
-                        }}
-                        InputProps={{
-                            style: {
-                                borderRadius: '8px',
-                                backgroundColor: '#FFFFFF',
-                            }
-                        }}
-                    />
-                </Grid>
+                <Stack direction="row" justifyContent="center" alignItems="center" className='w-100 mt-5'>
+                    <Card className='p-5'>
+                        <Grid item xs={12}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={12}>
+                                    <CssTextField
+                                        id="outlined-number"
+                                        placeholder="User Name Here..."
+                                        variant="outlined"
+                                        label="User Name Here..."
+                                        className='mt-0'
+                                        style={{ width: '100%' }}
+                                        InputLabelProps={{
+                                            style: { color: '#777777', fontSize: '14px' },
+                                        }}
+                                        InputProps={{
+                                            style: {
+                                                borderRadius: '8px',
+                                                backgroundColor: '#FFFFFF',
+                                            }
+                                        }}
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <CssTextField
+                                        id="outlined-number"
+                                        placeholder="Phone Number..."
+                                        variant="outlined"
+                                        label="Phone Number..."
+                                        className='mt-0'
+                                        style={{ width: '100%' }}
+                                        InputLabelProps={{
+                                            style: { color: '#777777', fontSize: '14px' },
+                                        }}
+                                        InputProps={{
+                                            style: {
+                                                borderRadius: '8px',
+                                                backgroundColor: '#FFFFFF',
+                                            }
+                                        }}
+                                    />
+                                </Grid>
+
+                                <Stack direction="row" justifyContent="end" alignItems="center" className='mt-4 w-100'>
+                                    <Button size="small" variant="contained" sx={{
+                                        width: 'auto', fontWeight: '600', padding: '10px 30px', fontSize: '16px',
+                                        backgroundColor: '#D9822B', borderRadius: '8px', textTransform: 'capitalize',
+                                        '&:hover': {
+                                            backgroundColor: '#C33332',
+                                        },
+                                    }}>
+                                        Submit
+                                    </Button>
+                                </Stack>
+
+                            </Grid>
+                        </Grid>
+                    </Card>
+                </Stack>
             </Grid>
 
 
-            <Stack direction="row" justifyContent="end" alignItems="center" className='mt-4'>
-                <Button size="small" variant="contained" sx={{
-                    width: 'auto', fontWeight: '600', padding: '10px 30px', fontSize: '16px',
-                    backgroundColor: '#D9822B', borderRadius: '8px', textTransform: 'capitalize',
-                    '&:hover': {
-                        backgroundColor: '#C33332',
-                    },
-                }}>
-                    Submit
-                </Button>
-            </Stack>
 
 
         </>
