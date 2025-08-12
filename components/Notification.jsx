12f7a6b2896d824edd5281c3a661cd7a7c8919a2
leofacts26 <leofacts26@gmail.com>
2025-08-12 19:44:14 +0530

    notification fixes

--- components/Notification.jsx
+++ components/Notification.jsx
@@ -108,63 +108,68 @@ const Notification = () => {
                             </Stack>
                             {/* <p className='notification-mark'>Mark all as read</p> */}
                         </Stack>
-                        <Divider sx={{ marginBottom: '30px' }} />
-                        {userNotifications?.map((item, index) => (
-                            <div style={{ padding: '10px', width: '400px' }} key={item.id || index}>
+                        <Divider sx={{ marginBottom: '10px' }} />
+                        {userNotifications && userNotifications.length > 0 ? (
+                            userNotifications.map((item, index) => (
+                                <div style={{ padding: '10px', width: '400px' }} key={item.id || index}>
+                                    <Stack direction="row" justifyContent="space-between">
+                                        {/* Left side - avatar & username */}
+                                        <Stack direction="row" alignItems="center" spacing={1}>
+                                            <Avatar
+                                                alt={item.username}
+                                                src="https://mui.com/static/images/avatar/1.jpg"
+                                                sx={{ width: 40, height: 40 }}
+                                            />
+                                            <Stack flexDirection="column">
+                                                <p className='text-dark notification-name'>{item.username}</p>
+                                                <p className='notification-username'>@{item.phone_number}</p>
+                                            </Stack>
+                                        </Stack>
 
-                                <Stack direction="row" justifyContent="space-between">
-                                    {/* Left side - avatar & username */}
-                                    <Stack direction="row" alignItems="center" spacing={1}>
-                                        <Avatar
-                                            alt={item.username}
-                                            src="https://mui.com/static/images/avatar/1.jpg"
-                                            sx={{ width: 40, height: 40 }}
-                                        />
-                                        <Stack flexDirection="column">
-                                            <p className='text-dark notification-name'>{item.username}</p>
-                                            <p className='notification-username'>@{item.phone_number}</p>
+                                        {/* Right side - date & read status */}
+                                        <Stack direction="row" alignItems="center" spacing={1}>
+                                            <p className='notification-date'>
+                                                {moment(item.created_at).format("MMM. Do, h:mma")}
+                                            </p>
+                                            {item.is_read === 0 && <span className="notification-red-dot"></span>}
                                         </Stack>
                                     </Stack>
 
-                                    {/* Right side - date & read status */}
-                                    <Stack direction="row" alignItems="center" spacing={1}>
-                                        <p className='notification-date'>
-                                            {moment(item.created_at).format("MMM. Do, h:mma")}
-                                        </p>
-                                        {item.is_read === 0 && <span className="notification-red-dot"></span>}
-                                    </Stack>
-                                </Stack>
-
-                                {/* Notification message */}
-                                <div>
-                                    <p
-                                        className="notification-para"
-                                        style={{
-                                            display: "-webkit-box",
-                                            WebkitLineClamp: expandedIndex === index ? "unset" : 3,
-                                            WebkitBoxOrient: "vertical",
-                                            overflow: "hidden"
-                                        }}
-                                    >
-                                        {item.message}
-                                    </p>
-
-                                    {item.message.length > 100 && ( // only show toggle if long
-                                        <span
+                                    {/* Notification message */}
+                                    <div>
+                                        <p
                                             className="notification-para"
-                                            onClick={() => toggleExpand(index)}
                                             style={{
-                                                cursor: "pointer",
-                                                color: "#c33332"
+                                                display: "-webkit-box",
+                                                WebkitLineClamp: expandedIndex === index ? "unset" : 3,
+                                                WebkitBoxOrient: "vertical",
+                                                overflow: "hidden"
                                             }}
                                         >
-                                            {expandedIndex === index ? "Show less" : "Show more"}
-                                        </span>
-                                    )}
-                                </div>
+                                            {item.message}
+                                        </p>
 
-                            </div>
-                        ))}
+                                        {item.message.length > 100 && (
+                                            <span
+                                                className="notification-para"
+                                                onClick={() => toggleExpand(index)}
+                                                style={{
+                                                    cursor: "pointer",
+                                                    color: "#c33332"
+                                                }}
+                                            >
+                                                {expandedIndex === index ? "Show less" : "Show more"}
+                                            </span>
+                                        )}
+                                    </div>
+                                </div>
+                            ))
+                        ) : (
+                            <p style={{ padding: "10px", textAlign: "center", color: "#666" }}>
+                                No Notification Found
+                            </p>
+                        )}
+
                     </Menu>
                 </div>
 
